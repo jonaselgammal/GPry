c974e0fe604ea293fb375f8442c2ec200c8bcb8e
jonaselgammal <jonas.el.gammal@rwth-aachen.de>
2021-08-17 17:21:30 +0200

    Added the correct imports

--- examples/simple_example_with_run.py
+++ examples/simple_example_with_run.py
@@ -1,5 +1,9 @@
+from gpry.run import run, mcmc
+from gpry.convergence import KL_from_draw_approx
+import numpy as np
 from scipy.stats import multivariate_normal, random_correlation
-from gpry.convergence import KL_from_draw
+from cobaya.model import get_model
+
 n_d = 3
 input_params = [f"x_{d}" for d in range(n_d)]
 
@@ -12,21 +16,22 @@ mean = np.zeros_like(std)
 
 rv = multivariate_normal(mean, cov)
 
+
 def f(**kwargs):
     X = [kwargs[p] for p in input_params]
     return np.log(rv.pdf(X))
 
+
 # Define the likelihood and the prior of the model
 info = {"likelihood": {"f": {"external": f,
                              "input_params": input_params}}}
-# {"external": Lkl,
-#                              "requires": {"n_d": n_d}}
 param_dict = {}
 for d, p_d in enumerate(input_params):
-    param_dict[p_d] = {"prior": {"min": mean[d]-5*std[d], "max": mean[d]+5*std[d]}}
+    param_dict[p_d] = {
+        "prior": {"min": mean[d]-5*std[d], "max": mean[d]+5*std[d]}}
 info["params"] = param_dict
 
 model = get_model(info)
-opt = {"n_points_per_acq" : 2}
+opt = {"n_points_per_acq": 2}
 conv = KL_from_draw_approx(model.prior, {})
 m, gp, acq, conv, opt = run(model, convergence_criterion=conv, callback="test")
