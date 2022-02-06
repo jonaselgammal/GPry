d188ed62bac44a713ab2688d0bf61e039c9233a2
jonaselgammal <jonas.el.gammal@rwth-aachen.de>
2022-02-06 18:33:38 +0100

    FIX: Kernel couldn't handle dynamic lenght scales in 1d

--- gpry/kernels.py
+++ gpry/kernels.py
@@ -173,21 +173,22 @@ class Kernel(sk_Kernel):
                     thetas = params[hyperparameter.name]
                     if np.iterable(thetas):
                         for t, theta in enumerate(thetas):
-                            if hyperparameter.max_length is None:
+                            if hyperparameter.max_length[t] is None:
                                 bounds.append([theta*1e-2, theta*1e2])
                             else:
                                 bounds.append(
                                     [hyperparameter.max_length[t]*1e-5,
-                                     hyperparameter.max_length[t]*10])
+                                     hyperparameter.max_length[t]])
                     else:
-                        if hyperparameter.max_length is None:
+                        if hyperparameter.max_length[0] is None:
                             bounds.append([thetas*1e-2, thetas*1e2])
                         else:
-                            bounds.append([hyperparameter.max_length*1e-5,
-                                           hyperparameter.max_length*10])
+                            bounds.append([hyperparameter.max_length[0]*1e-5,
+                                           hyperparameter.max_length[0]])
                 else:
                     bounds.append(hyperparameter.bounds)
         if len(bounds) > 0:
+            print(bounds)
             return np.log(np.vstack(bounds))
         else:
             return np.array([])
