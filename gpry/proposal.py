b55336381b4949258f0c5ef50a9062cbb5478bac
schoeneberg <nils.science@gmail.com>
2022-04-29 16:13:28 +0200

    Speed improvements as mentioned by Jesus

--- gpry/proposal.py
+++ gpry/proposal.py
@@ -14,13 +14,14 @@ class Proposer(metaclass=ABCMeta):
 
 class UniformProposer(Proposer):
     def __init__(self,bounds,n_d):
-        self.proposal_function = partial(scipy.stats.uniform.rvs,loc=bounds[:,0],scale=bounds[:,1],size=n_d)
+        proposal_pdf = scipy.stats.uniform(loc=bounds[:,0],scale=bounds[:,1])
+        self.proposal_function = partial(proposal_pdf.rvs,size=n_d)
     def get(self,random_state=None):
         return self.proposal_function(random_state=random_state)
-      
+
 class MeanCovProposer(Proposer):
     def __init__(self,mean, cov):
-        self.proposal_function = partial(scipy.stats.multivariate_normal.rvs,mean=mean,cov=cov)
+        self.proposal_function = scipy.stats.multivariate_normal(mean=mean,cov=cov).rvs
     def get(self,random_state=None):
         return self.proposal_function(random_state=random_state)
 
@@ -28,7 +29,7 @@ class MeanAutoCovProposer(Proposer):
     def __init__(self,mean,model_info):
         cmat_dir = get_best_covmat(model_info, packages_path=resolve_packages_path())
         if np.any(d!=0 for d in cmat_dir['covmat'].shape):
-          self.proposal_function = partial(scipy.stats.multivariate_normal.rvs,mean=mean,cov=cmat_dir['covmat'])
+          self.proposal_function = scipy.stats.multivariate_normal(mean=mean,cov=cmat_dir['covmat']).rvs
         else:
           self.proposal_function = model.prior.sample
     def get(self,random_state=None):
