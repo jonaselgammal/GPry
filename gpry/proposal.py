804f724a2cf84b8f6678b3b86802e52072d3088a
schoeneberg <nils.science@gmail.com>
2022-04-29 14:44:21 +0200

    Implemented the proposer from Jonas, which samples parts of the points randomly rather than with a technique

--- gpry/proposal.py
+++ gpry/proposal.py
@@ -3,6 +3,7 @@ from cobaya.cosmo_input.autoselect_covmat import get_best_covmat
 from cobaya.tools import resolve_packages_path
 from functools import partial
 import scipy.stats
+import numpy.random.random as random_draw
 
 class Proposer(metaclass=ABCMeta):
 
@@ -32,3 +33,23 @@ class MeanAutoCovProposer(Proposer):
     def get(self,random_state=None):
         return self.proposal_function(random_state=random_state)  
 
+class PartialProposer(Proposer):
+
+    # Either sample from true_proposer, or give a random prior sample
+    def __init__(self,random_proposal_fraction = 0., bounds, n_d,true_proposer):
+
+        if random_proposal_fraction > 1. or random_proposal_fraction < 0.:
+          raise ValueError("Cannot pass a fraction outside of [0,1]. You passed {} for 'random_proposal_fraction'.".format(random_proposal_fraction))
+        if not isinstance(true_proposer,Proposer):
+          raise ValueError("The true proposer needs to be a valid proposer.")
+
+        self.rpf = random_proposal_fraction
+        self.random_proposer = UniformProposer(bounds, n_d)
+        self.true_proposer = true_proposer
+
+    def get(self,random_state=None):
+        if random_draw() > self.rpf:
+            return self.true_proposer.get(random_state=random_state)
+        else:
+            return self.random_proposer.get(random_state=random_state)
+              
