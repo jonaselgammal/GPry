5e55847ca75de2bccda804e502379cd92b0bb4fa
jonaselgammal <jonas.el.gammal@rwth-aachen.de>
2022-06-23 18:05:21 +0200

    Getting ready for release...

--- setup.py
+++ setup.py
@@ -2,37 +2,42 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+from os import path
+
+def get_long_description():
+    """Get the long description from the README file."""
+    with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'),
+              encoding='utf-8') as f:
+        lines = f.readlines()
+        return "".join(lines[:])
+
 
 setup(
     name='gpry',
-    version='0.0.1',
-    description='Python GPry Package',
-    long_description="""The documentation can be found
-    at ...""",
+    version='1.0.0',
+    description='A package for fast bayesian inference of expensive Likelihoods',
+    long_description=get_long_description(),
     long_description_content_type='text/markdown',
-    author='Jonas El Gammal',
+    author='Jonas El Gammal, Jesus Torrado, Nils Schoeneberg and Christian Fidler',
     author_email='jonas.el.gammal@rwth-aachen.de',
-    license='MIT',
-    keywords='Gaussian Processes',
-    url='...',
+    license='LGPL',
+    keywords=['inference', 'gaussianprocesses', 'sampling', 'cosmology'],
+    url='https://github.com/jonaselgammal/GPry',
     project_urls={
         'Documentation':
-        '...',
-        'Source': '...',
+        'https://gpry.readthedocs.io',
+        'Source': 'https://github.com/jonaselgammal/GPry',
         },
     packages=['gpry'],
-    package_data={'gpry': ['data/*']},
     classifiers=[
-        'Development Status :: 4 - alpha',
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        ],
-    install_requires=['numpy', 'scikit-learn', 'dill', 'cobaya']
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+    install_requires=['cobaya', 'scikit-learn', 'dill']
 )
