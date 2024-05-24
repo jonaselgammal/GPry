7ca83d7632ff28592038cd9e60f91f8cb4a7f8c1
JesusTorrado <JesusTorrado@users.noreply.github.com>
2024-05-24 10:08:03 +0200

    diag: finer choices

--- gpry/diag.py
+++ gpry/diag.py
@@ -24,7 +24,9 @@ do_plot_slices = True
 
 
 def diagnosis(runner):
-    if mpi.is_main_process and do_check_inf_classifier and runner.gpr.infinities_classifier:
+    if not mpi.is_main_process:
+        return
+    if do_check_inf_classifier and runner.gpr.infinities_classifier:
         print("**************************************************")
         print("Traning set (full):")
         points = pd.DataFrame(
@@ -88,20 +90,22 @@ def diagnosis(runner):
             print("    SUBTEST: are finite points missing from GP?", all_finite_from_full_in_GP)
             print("    SUBTEST: are there more points in GP and finite?", same_length_finite_and_GP)
 
-        # PLOTS ##########################################################################
-        # Create the plots path, just in case it want not there yet
-        from gpry.io import create_path
-        import matplotlib.pyplot as plt
-        create_path(runner.plots_path)
-        # Get the "reference" MC sample from runner.reference, if set.
-        reference = getattr(runner, "reference", None)
-        fiducial = getattr(runner, "fiducial", None)
+    # PLOTS ##########################################################################
+    # Create the plots path, just in case it want not there yet
+    from gpry.io import create_path
+    import matplotlib.pyplot as plt
+    create_path(runner.plots_path)
+    # Get the "reference" MC sample from runner.reference, if set.
+    reference = getattr(runner, "reference", None)
+    fiducial = getattr(runner, "fiducial", None)
 
-        # Plot points distribution and convergence criterion
-        from gpry.plots import plot_points_distribution
+    # Plot points distribution and convergence criterion
+    from gpry.plots import \
+        plot_points_distribution, plot_slices, plot_slices_reference, \
+        getdist_add_training, _plot_2d_model_acquisition_std
+    # Do not plot if sample reweighted, to save time
+    if not runner.acquisition.is_last_MC_reweighted:
         try:
-            if runner.acquisition.is_last_MC_reweighted:
-                raise ValueError()
             plot_points_distribution(
                 runner.model, runner.gpr, runner.convergence,
                 runner.progress,
@@ -112,99 +116,96 @@ def diagnosis(runner):
             plt.savefig(os.path.join(runner.plots_path, "points_dist.svg"))
         plt.close()
 
-        # Plot mean GP and acq func slices
-        from gpry.plots import plot_slices, plot_slices_reference
-        if do_plot_slices:
-            plot_slices(
-                runner.model, runner.gpr, runner.acquisition, reference=reference
+    # Plot mean GP and acq func slices
+    if do_plot_slices:
+        plot_slices(
+            runner.model, runner.gpr, runner.acquisition, reference=reference
+        )
+        plt.savefig(os.path.join(
+            runner.plots_path,
+            f"slices_iteration_{runner.current_iteration:03d}.png")
+        )
+        plt.close()
+        try:
+            plot_slices_reference(
+                runner.model, runner.gpr, fiducial, truth=True, reference=reference,
             )
             plt.savefig(os.path.join(
                 runner.plots_path,
-                f"slices_iteration_{runner.current_iteration:03d}.png")
+                f"comparison_slices_iteration_{runner.current_iteration:03d}.png")
             )
-            plt.close()
-            try:
-                plot_slices_reference(
-                    runner.model, runner.gpr, fiducial, truth=True, reference=reference,
-                )
-                plt.savefig(os.path.join(
-                    runner.plots_path,
-                    f"comparison_slices_iteration_{runner.current_iteration:03d}.png")
-                )
-            except:
-                raise
-                pass
-            plt.close()
+        except:
+            raise
+            pass
+        plt.close()
 
-        # Plot current MC sample (if available)
-        from gpry.gp_acquisition import NORA
-        if (
-            do_plot_mc and isinstance(runner.acquisition, NORA) and
-            not runner.acquisition.is_last_MC_reweighted
-        ):
-            from getdist import plots
-            from getdist.mcsamples import MCSamplesError
-            from getdist.densities import DensitiesError
-            mcsamples = runner.acquisition.last_MC_sample_getdist(runner.model)
-            to_plot = [mcsamples]
-            to_plot_params = list(runner.model.parameterization.sampled_params())
-            name_logp, label_logp = "logpost", r"\log(p)"
-            if mcsamples.loglikes is not None:
-                to_plot_params += [name_logp]
-            if reference is not None:
-                if reference.loglikes is not None:
-                    try:
-                        # PROBLEM: loglikes will be -chi2 if obtained with PolyChord and
-                        # not corrected, but we cannot assume anything here: it may have
-                        # been corrected already
-                        reference.addDerived(
-                            -reference.loglikes, name_logp, label=label_logp,
-#                            range=(-np.inf, max(-reference.loglikes)),
-                        )
-                    except ValueError:  # already added
-                        pass
-                else:
-                    # Limitation: if logp not present in reference MCSamples (rare), can
-                    # not plot for GP either (1st sample needs to contain all params)
-                    if to_plot_params[-1] == name_logp:
-                        to_plot_params = to_plot_params[:-1]
-            filled = [True]
-            legend_labels = [f"Last NORA sample ({len(runner.gpr.X_train_all)} evals.)"]
-            if reference is not None:
-                to_plot = [reference] + to_plot
-                filled = [False] + filled
-                legend_labels += ["Reference sample"]
-            g = plots.get_subplot_plotter(subplot_size=2)
-            try:
-                g.triangle_plot(
-                    to_plot, to_plot_params, filled=filled, legend_labels=legend_labels
-                )
+    # Plot current MC sample (if available)
+    from gpry.gp_acquisition import NORA
+    if (
+        do_plot_mc and isinstance(runner.acquisition, NORA) and
+        not runner.acquisition.is_last_MC_reweighted
+    ):
+        from getdist import plots
+        from getdist.mcsamples import MCSamplesError
+        from getdist.densities import DensitiesError
+        mcsamples = runner.acquisition.last_MC_sample_getdist(runner.model)
+        to_plot = [mcsamples]
+        to_plot_params = list(runner.model.parameterization.sampled_params())
+        name_logp, label_logp = "logpost", r"\log(p)"
+        if mcsamples.loglikes is not None:
+            to_plot_params += [name_logp]
+        if reference is not None:
+            if reference.loglikes is not None:
                 try:
-                    from gpry.plots import getdist_add_training
-                    getdist_add_training(g, runner.model, runner.gpr, highlight_last=True)
-                except Exception as e:
-                    raise
-                    print(f"FAILED ADDING TRAINING POINTS! Error msg: {e}")
+                    # PROBLEM: loglikes will be -chi2 if obtained with PolyChord and
+                    # not corrected, but we cannot assume anything here: it may have
+                    # been corrected already
+                    reference.addDerived(
+                        -reference.loglikes, name_logp, label=label_logp,
+                         range=(-np.inf, max(-reference.loglikes)),
+                    )
+                except ValueError:  # already added
+                    pass
+            else:
+                # Limitation: if logp not present in reference MCSamples (rare), can
+                # not plot for GP either (1st sample needs to contain all params)
+                if to_plot_params[-1] == name_logp:
+                    to_plot_params = to_plot_params[:-1]
+        filled = [True]
+        legend_labels = [f"Last NORA sample ({len(runner.gpr.X_train_all)} evals.)"]
+        if reference is not None:
+            to_plot = [reference] + to_plot
+            filled = [False] + filled
+            legend_labels += ["Reference sample"]
+        g = plots.get_subplot_plotter(subplot_size=2)
+        try:
+            g.triangle_plot(
+                to_plot, to_plot_params, filled=filled, legend_labels=legend_labels
+            )
+            try:
+                getdist_add_training(g, runner.model, runner.gpr, highlight_last=True)
+            except Exception as e:
+                raise
+                print(f"FAILED ADDING TRAINING POINTS! Error msg: {e}")
+            plt.savefig(
+                os.path.join(
+                    runner.plots_path,
+                    f"NORA_iteration_{runner.current_iteration:03d}.png"))
+            plt.close()
+        except (ValueError, IndexError, AttributeError, np.linalg.LinAlgError, MCSamplesError, DensitiesError) as e:
+            print(f"COULD NOT DO TRIANGLE PLOT! Reason: {e}")
+
+        if runner.model.prior.d() == 2:
+            try:
+                _plot_2d_model_acquisition_std(
+                    runner.gpr, runner.acquisition, last_points=None, res=200
+                )
                 plt.savefig(
                     os.path.join(
                         runner.plots_path,
-                        f"NORA_iteration_{runner.current_iteration:03d}.png"))
+                        f"contours_iteration_{runner.current_iteration:03d}.png"))
                 plt.close()
-            except (ValueError, IndexError, AttributeError, np.linalg.LinAlgError, MCSamplesError, DensitiesError) as e:
-                print(f"COULD NOT DO TRIANGLE PLOT! Reason: {e}")
+            except ValueError as e:
+                print(f"COULD NOT PLOT CONTOURS! Reason: {e}")
 
-            if runner.model.prior.d() == 2:
-                from gpry.plots import _plot_2d_model_acquisition_std
-                try:
-                    _plot_2d_model_acquisition_std(
-                        runner.gpr, runner.acquisition, last_points=None, res=200
-                    )
-                    plt.savefig(
-                        os.path.join(
-                            runner.plots_path,
-                            f"contours_iteration_{runner.current_iteration:03d}.png"))
-                    plt.close()
-                except ValueError as e:
-                    print(f"COULD NOT PLOT CONTOURS! Reason: {e}")
-
-        print("**************************************************")
+    print("**************************************************")
