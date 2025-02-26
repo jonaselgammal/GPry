f091ad49c8eb5689b6191aea5b9b0cf69ffa4bac
JesusTorrado <JesusTorrado@users.noreply.github.com>
2025-02-26 10:46:29 +0100

    docs: favicon

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
+    'sphinx_favicon',
 ]
 
 # -- Extensions to the  Napoleon GoogleDocstring class ---------------------
@@ -179,10 +180,13 @@ html_logo = 'images/logo_clear_bkg_motto.png'
 # pixels large.
 #html_favicon = None
 
+# Using the sphinx_favicon package instead to be able to use .svg
+favicons = ["favicon.svg"]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['images/favicon']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
